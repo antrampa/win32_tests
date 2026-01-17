908ed07e82ec5c310400b095a3c69001dc46ea60
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-01-17 23:30:46 +0200

    Added Keyboard tracker

--- MouseTracker/main.cpp
+++ MouseTracker/main.cpp
@@ -1,16 +1,20 @@
-#define UNICODE
-#define _UNICODE
+// #define UNICODE
+// #define _UNICODE
+// g++ main.cpp -o MouseTracker.exe -DUNICODE -D_UNICODE -mwindows -lgdi32 -luser32
 #include <windows.h>
 #include <string>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
 
 HHOOK g_mouseHook;
 HWND g_hwnd;
+HHOOK g_hKeyboardHook;
 
 HWND hLabelX;
 HWND hLabelY;
+HWND hLabelKey;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
 
@@ -29,7 +33,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
         CLASS_NAME,
         L"Global Mouse Tracker",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 350, 200,
+        CW_USEDEFAULT, CW_USEDEFAULT, 450, 250,
         NULL,
         NULL,
         hInstance,
@@ -42,6 +46,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
 
     // Install global mouse hook
     g_mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, NULL, 0);
+
+    // Install global keyboard hook
+    g_hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
 
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
@@ -62,23 +69,33 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     {
         CreateWindow(L"STATIC", L"Mouse X:",
             WS_VISIBLE | WS_CHILD,
-            30, 40, 80, 25,
+            30, 40, 180, 25,
             hwnd, NULL, NULL, NULL);
 
         CreateWindow(L"STATIC", L"Mouse Y:",
             WS_VISIBLE | WS_CHILD,
-            30, 80, 80, 25,
+            30, 80, 180, 25,
+            hwnd, NULL, NULL, NULL);
+        
+        CreateWindow(L"STATIC", L"Keyboard Key Code:", 
+            WS_VISIBLE | WS_CHILD,
+            30, 120, 180, 25,
             hwnd, NULL, NULL, NULL);
 
         hLabelX = CreateWindow(L"STATIC", L"0",
             WS_VISIBLE | WS_CHILD | WS_BORDER,
-            120, 40, 150, 25,
+            200, 40, 200, 25,
             hwnd, NULL, NULL, NULL);
 
         hLabelY = CreateWindow(L"STATIC", L"0",
             WS_VISIBLE | WS_CHILD | WS_BORDER,
-            120, 80, 150, 25,
+            200, 80, 200, 25,
             hwnd, NULL, NULL, NULL);
+
+        hLabelKey = CreateWindow(L"STATIC", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER,
+            200, 120, 200, 25,
+            hwnd, NULL, NULL, NULL);            
     }
     break;
 
@@ -103,4 +120,17 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
     }
 
     return CallNextHookEx(g_mouseHook, nCode, wParam, lParam);
+}
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+
+        if (wParam == WM_KEYDOWN) {
+            printf("Key pressed: %d\n", p->vkCode);
+            std::wstring keyText = L"" + std::to_wstring(p->vkCode);
+            SetWindowText(hLabelKey, keyText.c_str());
+        }
+    }
+    return CallNextHookEx(g_hKeyboardHook, nCode, wParam, lParam);
 }
